5de975a66201d86d291d83511977c25c4487b1ff
stratosefty <stratosefty@gmail.com>
2022-06-14 23:00:35 +0200

    Revert "small changes"
    This reverts commit fb73471888fe7feb63707ed34e0b6c6b8f9ac99f.

--- client.py
+++ client.py
@@ -1,15 +1,9 @@
-
 import socket
-import os
-HOST = "131.155.246.24"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-    while True:
-        data = s.recv(1024)
-        print(data)
-        if data== b's':
-            print("Get Fucked")
-        if data== b'cmd':
-            print("open command promptS")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+while (True):
+    msg = s.recv(1024)
+    decodedMsg = msg.decode("utf-8")
+    print(decodedMsg)
