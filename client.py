a6b7f45ed5c5abad9a0f087b88e49955850388ae
stratosefty <stratosefty@gmail.com>
2022-06-14 19:35:13 +0200

    Basic socket client and host code

--- client.py
+++ client.py
@@ -1,14 +1,7 @@
-# echo-client.py
-
 import socket
-import os
-HOST = "131.155.246.24"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-    #while True:
-        data = s.recv(1024)
-        print(data)
-        if data== b's':
-            os.system("shutdown /s /t 1")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,23 +1,11 @@
-# echo-server.py
-import os
-
 import socket
 
-HOST = "145.116.40.250"  # Standard loopback interface address (localhost)
-PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print ("Server started")
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            print (data)
-            if not data:
-                break
-            if data == (b"shutdown"):
-                os.system("shutdown /s /t 1")
-            conn.sendall(data)
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+    clientsocket.send(bytes("Hey there!!!","utf-8"))
