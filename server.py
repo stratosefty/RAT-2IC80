97bf05c129eebdc37030259eb9c94b6c784d9d9d
stratosefty <stratosefty@gmail.com>
2022-06-14 19:41:25 +0200

    Very basic working message exchange

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from tokenize import String
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -8,4 +9,5 @@ while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!","utf-8"))
+    msgToSend = input("Enter your message: ")
+    clientsocket.send(bytes(msgToSend,"utf-8"))
